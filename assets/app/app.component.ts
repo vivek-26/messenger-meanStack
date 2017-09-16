441ffd00e78db7ca5e3995664c94ea9566d142f1
Vivek Kumar <vivek.kumar26@live.com>
2017-09-16 18:10:19 +0530

    data binding and event emitters

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+import { Message } from "./messages/message.model";
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
+   selector: 'my-app',
+   templateUrl: './app.component.html'
 })
 export class AppComponent {
-    
+   message: Message = new Message('This is a sample message!', 'Vivek Kumar')
 }
