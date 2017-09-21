ebfa660417102026d387a8c64e484be3f528c9a5
Vivek Kumar <vivek.kumar26@live.com>
2017-09-21 13:15:17 +0530

    add child routes to auth route

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
    selector: 'app-authentication',
-   template: `<h1>Authentication</h1>`
+   template: `
+      <header class="row spacing">
+         <nav class="col-md-8 col-md-offset-2">
+            <ul class="nav nav-tabs">
+               <li routerLinkActive="active"><a [routerLink]="['signup']">Sign-Up</a></li>
+               <li routerLinkActive="active"><a [routerLink]="['signin']">Sign-In</a></li>
+               <li routerLinkActive="active"><a [routerLink]="['logout']">Logout</a></li>
+            </ul>
+         </nav>
+      </header>
+      <div class="row spacing">
+         <router-outlet></router-outlet>
+      </div>
+   `
 })
 
 export class AuthenticationComponent { }
--- assets/app/auth/logout.component.ts
+++ assets/app/auth/logout.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
    selector: 'app-logout',
    template: `
       <div class="col-md-8 col-md-offset-2">
-         <button class="btn btn-danger" (click)="onLogout()"></button>
+         <button class="btn btn-danger" (click)="onLogout()">Logout</button>
       </div>
    `
 })
