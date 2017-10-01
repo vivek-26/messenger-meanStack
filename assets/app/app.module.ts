3125c43f8c80115e231c34f48f2734fe3f4a4338
Vivek Kumar <vivek.kumar26@live.com>
2017-10-01 19:47:11 +0530

    auth module & lazy loading

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -8,9 +7,6 @@ import { MessageModule } from './messages/message.module';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
 import { routing } from './app.routing';
-import { SigninComponent } from './auth/signin.component';
-import { SignupComponent } from './auth/signup.component';
-import { LogoutComponent } from './auth/logout.component';
 import { AuthService } from './auth/auth.service';
 import { ErrorComponent } from './errors/error.component';
 import { ErrorService } from './errors/error.service';
@@ -21,14 +17,10 @@ import { ErrorService } from './errors/error.service';
       AppComponent,
       AuthenticationComponent,
       HeaderComponent,
-      SignupComponent,
-      SigninComponent,
-      LogoutComponent,
       ErrorComponent
    ],
    imports: [BrowserModule,
       routing,
-      ReactiveFormsModule,
       HttpModule,
       MessageModule
    ],
---
+++ assets/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { LogoutComponent } from './logout.component';
+import { SignupComponent } from './signup.component';
+import { SigninComponent } from './signin.component';
+import { authRouting } from './auth.routing';
+
+@NgModule({
+   declarations: [
+      SigninComponent,
+      SignupComponent,
+      LogoutComponent
+   ],
+   imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      authRouting
+   ]
+})
+export class AuthModule { }
